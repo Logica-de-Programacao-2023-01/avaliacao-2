3223e0f278fe14fb6dc4eb203fa08d578eb5bbe5
gadelima <gabriel.delima@mercadolibre.com>
2023-05-25 20:00:21 -0300

    Initial commit

---
+++ bonus/bonus.go
@@ -0,0 +1,18 @@
+package bonus
+
+//Implemente uma função que inverta
+//uma [lista encadeada](https://www.ime.usp.br/~pf/algoritmos/aulas/lista.html#:~:text=Uma%20lista%20encadeada%20%C3%A9%20uma,segunda%2C%20e%20assim%20por%20diante),
+//ou seja, altere a ordem dos elementos da lista de forma reversa.
+
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+func InvertLinkedList(list *LinkedList) {
+
+}
